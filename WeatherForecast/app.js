095161b249333389f17531f0d6ea2ca3d6d25bc7
Christopher Pang <matthewstirbate@gmail.com>
2016-10-21 15:16:35 -0700

    updated routing for the app

--- WeatherForecast/app.js
+++ WeatherForecast/app.js
@@ -1,3 +1,15 @@
 angular.module('WeatherApp', ['ngRoute', 'WeatherApp'])
 
 
+.config(function ($routeProvider, $locationProvider){
+
+	$routeProvider
+		.when('/weather', {
+			controller: 'WeatherController',
+			templateUrl: 'src/weather.html'
+		})
+		.otherwise({
+			redirectTo: '/weather'
+		})
+
+});
