be28c47ffcecd6438aaa1c1ca7cf914b8d636549
Christopher Pang <matthewstirbate@gmail.com>
2016-10-23 19:00:35 -0700

    get today's date

--- WeatherForecast/controller.js
+++ WeatherForecast/controller.js
@@ -308,4 +308,120 @@ angular.module('WeatherApp')
 	$interval(tick, 1000);
 
 
+	$scope.switchTemp = function(param) {
+		if(param == '°C') {
+			$rootScope.unit = '°F';
+			$rootScope.currTemp = currTempF;
+
+			for (var i = 0; i < tempTemperatureF.length; i++){
+				$rootScope.forecastArray[i][1] = tempTemperatureF[i];
+			}
+
+		} else {
+			$rootScope.unit = '°C';
+			$rootScope.currTemp = currTempC;
+
+			for (var i = 0; i < tempTemperatureC.length; i++){
+				$rootScope.forecastArray[i][1] = tempTemperatureC[i];
+			}
+		}
+	}
+
+	$scope.getCurrDate = function(){
+		var today = new Date();
+		tempDay = today.getDay();
+		date = today.getDate();
+		tempMonth = today.getMonth();
+		var year = today.getFullYear();
+
+		var day = '';
+
+		switch (tempDay) {
+			case 0:
+			day = 'Sunday';
+			break;
+			case 1:
+			day = 'Monday';
+			break;
+			case 2:
+			day = 'Tuesday';
+			break;
+			case 3:
+			day = 'Wednesday';
+			break;
+			case 4:
+			day = 'Thursday';
+			break;
+			case 5:
+			day = 'Friday';
+			break;
+			case 6:
+			day = 'Saturday';
+			break;
+			default:
+			day = '';
+		}	
+
+		var month = '';
+
+		switch(tempMonth) {
+			case 0:
+			month = 'January';
+			break;
+
+			case 1:
+			month = 'February';
+			break;
+
+			case 2:
+			month = 'March';
+			break;
+
+			case 3:
+			month = 'April';
+			break;
+
+			case 4:
+			month = 'May';
+			break;
+
+			case 5:
+			month = 'June';
+			break;
+
+			case 6:
+			month = 'July';
+			break;
+
+			case 7:
+			month = 'August';
+			break;
+
+			case 8:
+			month = 'September';
+			break;
+
+			case 9:
+			month = 'October';
+			break;
+
+			case 10:
+			month = 'November';
+			break;
+
+			case 11 :
+			month = 'December';
+			break;
+
+			default:
+			month = '';
+		}
+
+		var currentDate = day + ', ' + month + ' ' + date + ' ' + year;
+		//console.log(currentDate);
+		$rootScope.dateToday = currentDate;
+
+	}
+
+
 });
